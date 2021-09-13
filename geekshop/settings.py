a491ca22399c7461db2d7823e693358b9b87d156
Lalluviadel <inspiracion@yandex.ru>
2021-09-13 10:58:54 +0300

    Lesson_01

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vc8ntgr^y%&$b8sk#bxzar3dbl$@51!(ky0)80d=dg95dbg1mo'
+SECRET_KEY = 'django-insecure-h4s&e1gikggpa+bf&)=7vw8@tjjl6j)o%$a@!l8%pi8dcnnuae'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
